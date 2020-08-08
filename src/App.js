cd9fc21ece96b8c947960e74d2bf5846602dd4b5
Demisew Mokonnen <demisewt@gmail.com>
2020-08-08 12:17:40 -0500

    committing docker-compose

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Wow this is react, Whaat?
+          Wow this is react, Whaat? Jaal? Obboleessoo?
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
